69b5c1224c3e619c057684d6206d7483493a4afd
IronBatman2715 <72224765+IronBatman2715@users.noreply.github.com>
2022-05-30 20:54:27 -0400

    Add stop shell script

--- stop.sh
+++ stop.sh
@@ -4,5 +4,5 @@ projectDir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 dockerComposeLocation="$projectDir/docker-compose.yml"
 
-# Start all containers defined in docker-compose.yml
-docker compose -f $dockerComposeLocation up -d
+# Stop all containers defined in docker-compose.yml
+docker compose -f $dockerComposeLocation down
